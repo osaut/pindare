e7d6fa31cee1cb775183bb1218e2f0d237165c9d
Olivier Saut <osaut@airpost.net>
2012-10-28 17:59:43 +0100

    Intégration temporelle

--- lib/integrator.rb
+++ lib/integrator.rb
@@ -1,13 +1,30 @@
+#encoding: utf-8
+# Module d'intégration temporel
 module TimeIntegrator
+    # Intégration par Euler explicite
+    #
+    # @param [NArray] y_tn Vecteur au temps précédent
+    # @param [FixNum] dt Pas de temps
+    # @return [NArray] vecteur au temps (n+1)dt
     def ts_euler_explicit(y_tn, dt)
         y_tn+dt*func(y_tn)
     end
 
+    # Intégration par Runge-Kutta d'ordre 2
+    #
+    # @param [NArray] y_tn Vecteur au temps précédent
+    # @param [FixNum] dt Pas de temps
+    # @return [NArray] vecteur au temps (n+1)dt
     def ts_RK2(y_tn,dt)
         y_dem=y_tn+0.5*dt*func(y_tn)
         y_tn+dt*func(y_dem)
     end
 
+    # Intégration par Runge-Kutta d'ordre 4
+    #
+    # @param [NArray] y_tn Vecteur au temps précédent
+    # @param [FixNum] dt Pas de temps
+    # @return [NArray] vecteur au temps (n+1)dt
     def ts_RK4(y_tn, dt)
         k1=func(y_tn)
         k2=func(y_tn+0.5*dt*k1)
